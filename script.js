e9472c149e19e0beec7da3801f935a1ee544774c
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-10-20 09:00:13 +0200

    Iteration#1

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// #1: Buscar el máximo
+function sum(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne);
+  } else {
+    console.log(numberTwo);
+  }
+}
+
+sum(7, 8); // Devuelve 8.
