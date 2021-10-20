f4fdbf4314fd3bd9ef4de01469be6ccd838d631a
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-10-20 09:34:12 +0200

    Iteracion#2

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function sum(numberOne, numberTwo) {
 }
 
 sum(7, 8); // Devuelve 8.
+
+// #2: Buscar la palabra más larga
+const avengers = ["Hulk", "Thor", "IronMan", "Captain A.", "Spiderman", "Captain M."];
+
+function findLongestWord(param) {
+  let avengersOrdenado = avengers.sort(function (primero, segundo) {
+    return siguiente.length - anterior.length;
+  });
+  return avengersOrdenado[0];
+}
+
+console.log(findLongestWord(avengers)); // devuelve Captain A.
